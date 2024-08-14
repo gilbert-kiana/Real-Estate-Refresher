5306fd64e9b263deb510ce879b16594de698a357
gilbert-kiana <gilbertintabo@gmail.com>
2024-08-14 04:25:35 +0300

    adding homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./layout.scss"
 import NavBar from "./components/navbar/NavBar"
+import HomePage from "./routes/homePage/HomePage"
 
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
       <NavBar/>
+      </div>
+      <div className="content">
+        <HomePage/>
+      </div>
     </div>
   )
 }
