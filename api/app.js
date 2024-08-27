857de28dd44d5b9ff97f4d940a3ee9b9efbfaebe
gilbert-kiana <gilbertintabo@gmail.com>
2024-08-27 13:24:50 +0300

    adding post and auth router

--- api/app.js
+++ api/app.js
@@ -1,6 +1,12 @@
 import express from "express"
+import postRouter from "./routes/post.route.js"
+import authRoute  from "./routes/auth.route.js"
 
 const app = express();
+
+app.use("/api/auth",authRoute)
+app.use("/api/posts", postRouter)
+
 
 app.listen(8800,()=>{
     console.log("Server is running!");
