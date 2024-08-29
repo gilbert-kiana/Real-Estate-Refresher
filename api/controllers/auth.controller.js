4afbdc1cadba30ad4a8c5f1a362a1022b6098d7c
gilbert-kiana <gilbertintabo@gmail.com>
2024-08-29 13:00:23 +0300

    auth controller register functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,26 @@
-export const register = (req,res)=>{
-    console.log("register endpoint")
+import bcrypt from "bcrypt"
+import prisma from "../lib/prisma";
+
+export const register =async (req,res)=>{
+    const {username,email,password} = req.body
+
+    try{
+        const hashedPassword =  await bcrypt.hash(password,10);
+     
+        const newUser = await prisma.user.create({
+            data:{
+                username,
+                email,
+                password:hashedPassword
+            }
+        })
+        console.log(newUser)
+
+        res.status(201).json({message:"user created successfully"});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:"Failed to create user!"})
+    }
 }
 
 export const login = (req,res)=>{
