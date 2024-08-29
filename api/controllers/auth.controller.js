a1cac1a70808335677c272c2d247e3906f780bf7
gilbert-kiana <gilbertintabo@gmail.com>
2024-08-29 03:32:23 +0300

    auth controller

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req,res)=>{
+    console.log("register endpoint")
+}
+
+export const login = (req,res)=>{
+    console.log("login endpoint")
+}
+
+export const logout =(req,res)=>{
+    console.log("logout endpoint")
+}
