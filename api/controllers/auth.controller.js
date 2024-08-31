0609019739f5af99776d52f50162393f4cbe3f90
gilbert-kiana <gilbertintabo@gmail.com>
2024-08-31 17:16:39 +0300

    login endpoint

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 import prisma from "../lib/prisma.js"
+import jwt from "jsonwebtoken"
 
 export const register =async (req,res)=>{
     const {username,email,password} = req.body
@@ -23,10 +24,43 @@ export const register =async (req,res)=>{
     }
 }
 
-export const login = (req,res)=>{
-    console.log("login endpoint")
+export const login = async (req,res)=>{
+   const {username,password} = req.body;
+
+   try {
+    const user = await prisma.user.findUnique({
+        where:{username},
+    })
+
+    if(!user) return res.status(400).json({message:"Invalid Credentials!"})
+
+    const isPasswordValid = await bcrypt.compare(password,user.password)
+
+    if(!isPasswordValid)
+        return res.status(400).json({message:"Invalid Credentials!"});
+
+    const age = 1000 * 60 * 60 * 24 * 7 ;
+
+    const token = jwt.sign(
+        {
+        id:user.id,
+        isAdmin:false
+    },
+    process.env.JWT_SECRET_KEY,
+    {expiresIn:age})
+
+    const {password:userPassword, ...userInfo}= user;
+
+    res.cookie("token",token,{
+        httponly:true,
+        maxAge:age
+    }).status(200).json(userInfo)
+   } catch (error) {
+    console.log(error);
+    res.status(500).json({message:"Failed to Login!"})
+   }
 }
 
 export const logout =(req,res)=>{
-    console.log("logout endpoint")
+   res.clearCookie("token").status(200).json({message:"Logout Successfull"})
 }
