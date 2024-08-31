fa5c321c6bb33f78047fd01b73b33444ab6c7778
gilbert-kiana <gilbertintabo@gmail.com>
2024-08-31 17:18:08 +0300

    pushing formdata from frontend to backend

--- frontend/src/routes/register/register.jsx
+++ frontend/src/routes/register/register.jsx
@@ -1,7 +1,40 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+import axios from "axios"
+import {useState} from "react"
+
+
 
 function Register() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("")
+    setIsLoading(true);
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <div className="register">
       <div className="formContainer">
