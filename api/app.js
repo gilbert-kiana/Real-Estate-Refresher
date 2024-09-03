a77be92352c841582fca1b92f094e24543c04fa9
gilbert-kiana <gilbertintabo@gmail.com>
2024-09-03 13:48:19 +0300

    changed cors config

--- api/app.js
+++ api/app.js
@@ -1,12 +1,16 @@
 import express from "express"
 import postRouter from "./routes/post.route.js"
 import authRoute  from "./routes/auth.route.js"
-import cors from cors;
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({origin:process.env.CLIENT_URL, credentials:true}))
+const corOptions = {
+    origin:process.env.CLIENT_URL,
+    credentials:true
+}
+app.use(cors(corOptions))
 app.use(express.json())
 app.use(cookieParser())
 
